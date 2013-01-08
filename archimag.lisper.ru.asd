58cb6015d9ce6f7c0693169d6b389bcd67708c02
Moskvitin Andrey <archimag@gmail.com>
2013-01-09 00:40:29 +0400

    update

--- archimag.lisper.ru.asd
+++ archimag.lisper.ru.asd
@@ -2,7 +2,9 @@
 
 (defsystem #:archimag.lisper.ru
   :defsystem-depends-on (#:arblog-systems #:closure-template)
-  :depends-on (#:arblog #:arblog-datastore-mongodb #:arblog-markup-rst #:arblog-theme-mirev)
+  :depends-on (#:arblog #:arblog-datastore-mongodb #:arblog-markup-rst
+               #:arblog-theme-mirev #:arblog-theme-isimple)
   :serial t
   :components ((:file "defmodule")
-               (:closure-template "archimag")))
+               (:closure-template "archimag-mirev")
+               (:closure-template "archimag-isimple")))
--- defmodule.lisp
+++ defmodule.lisp
@@ -9,6 +9,10 @@
  '#:arblog.theme.mirev_archimag.tmpl
  :prototype (closure-template:package-ttable '#:arblog.theme.mirev.tmpl))
 
+(closure-template:ensure-ttable-package
+ '#:arblog.theme.isimple_archimag.tmpl
+ :prototype (closure-template:package-ttable '#:arblog.theme.isimple.tmpl))
+
 (arblog:register-theme-static-dir
  "archimag"
  (merge-pathnames "static/" (asdf:component-pathname  (asdf:find-system '#:archimag.lisper.ru))))
@@ -16,12 +20,15 @@
 
 (restas:mount-module -arblog- (#:arblog)
   (arblog:*blog-name* "archimag")
+  (arblog:*blog-author* "Andrey Moskvitin")
   (arblog:*posts-on-page* 10)
   
   (arblog.internal.datastore:*datastore* (make-instance 'arblog.datastore.mongodb:arblog-mongo-datastore))
   (arblog.internal.markup:*markup* (make-instance 'arblog.markup.rst:arblog-rst-markup))
-  (arblog.internal.theme:*theme* (make-instance 'arblog.theme.mirev:arblog-mirev-theme
-                                                :templates-package '#:arblog.theme.mirev_archimag.tmpl))
+  ;; (arblog.internal.theme:*theme* (make-instance 'arblog.theme.mirev:arblog-mirev-theme
+  ;;                                               :templates-package '#:arblog.theme.mirev_archimag.tmpl))
+  (arblog.internal.theme:*theme* (make-instance 'arblog.theme.isimple:arblog-isimple-theme
+                                                :templates-package '#:arblog.theme.isimple_archimag.tmpl))
   
   (arblog:*disqus-shortname* "archimagblog")
   (arblog:*disqus-developer-mode* nil)
